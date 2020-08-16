02f1f09cb30c0a7b30cb6db940df697e1b1c2d1f
yl12510 <lewis8904@hotmail.com>
2020-08-16 22:46:54 +0100

    rename files

--- my_practice/run_network.py
+++ my_practice/run_network.py
@@ -1,7 +1,7 @@
-import network_matrix
+import network
 import mnist_loader
 
 if if __name__ == "__main__":
     training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
-    net = network_matrix.Network([784, 30, 10])
+    net = network.Network([784, 30, 10])
     net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
